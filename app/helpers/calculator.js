4e4fe70544cd8232728e48038dff842fb2036bfe
Thomas O'Leary <t.oleary@me.com>
2021-10-29 13:49:05 -0700

    update shots array

--- app/helpers/calculator.js
+++ app/helpers/calculator.js
@@ -1,6 +1,10 @@
 const shots = [
-  { club: "8 Iron", min: 150, max: 164, minPow: 50 },
+  { club: "60", min: 5, max: 25, minPow: 50 },
+  { club: "56", min: 26, max: 45, minPow: 50 },
+  { club: "52", min: 46, max: 80, minPow: 50 },
+  { club: "PW", min: 81, max: 139, minPow: 50 },
   { club: "9 Iron", min: 140, max: 159, minPow: 50 },
+  { club: "8 Iron", min: 150, max: 164, minPow: 50 },
   { club: "7 Iron", min: 165, max: 179, minPow: 50 },
   { club: "6 Iron", min: 180, max: 200, minPow: 50 },
 ];
