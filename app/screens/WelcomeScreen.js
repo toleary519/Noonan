dd443640eb085bbd9ff6c54c1180830151f08fe9
Thomas O'Leary <t.oleary@me.com>
2021-10-29 00:39:04 -0700

    bg color change

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -3,26 +3,20 @@ import React from "react";
 
 export default function WelcomeScreen({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.options}>
-        <Button onPress={() => navigation.navigate("Bag")} title="My Bag" />
-        <Button onPress={() => navigation.navigate("Shoot")} title="Shoot!" />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Button onPress={() => navigation.navigate("Bag")} title="My Bag" />
+      <Button onPress={() => navigation.navigate("Shoot")} title="Shoot!" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    backgroundColor: "#B68014",
-  },
-  options: {
     flexDirection: "row",
-    justifyContent: "space-evenly",
     alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fbf0da",
+    fontSize: 55,
   },
 });
