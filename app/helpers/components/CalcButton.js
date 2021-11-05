a3521dbeff210335443116a5a16d76314bc87a0e
Thomas O'Leary <t.oleary@me.com>
2021-11-04 17:00:46 -0700

    change calc button

--- app/helpers/components/CalcButton.js
+++ app/helpers/components/CalcButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default CalcButton = ({ text, onPress, special }) => (
-  <TouchableOpacity delayPressIn={150}>
+  <TouchableOpacity>
     <Text onPress={onPress} style={special ? styles.special : styles.digit}>
       {text}
     </Text>
@@ -11,17 +11,15 @@ export default CalcButton = ({ text, onPress, special }) => (
 
 const styles = StyleSheet.create({
   digit: {
-    // borderWidth: 1,
     textAlign: "center",
-    padding: 10,
-    fontSize: 55,
+    paddingTop: 4,
+    fontSize: 40,
     fontWeight: "bold",
-    color: "#2f8587",
   },
   special: {
     textAlign: "center",
-    padding: 10,
-    fontSize: 55,
+    paddingTop: 4,
+    fontSize: 40,
     fontWeight: "bold",
     color: "#FC440F",
   },
