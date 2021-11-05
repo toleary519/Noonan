a79d8d7d0d7a04137a9301c163e1d0eab845da6b
Thomas O'Leary <t.oleary@me.com>
2021-11-04 22:21:08 -0700

    update shots

--- app/helpers/calculator.js
+++ app/helpers/calculator.js
@@ -1,12 +1,12 @@
 const shots = [
-  { club: "60", min: 5, max: 25, minPow: 50 },
-  { club: "56", min: 26, max: 45, minPow: 50 },
-  { club: "52", min: 46, max: 80, minPow: 50 },
-  { club: "PW", min: 81, max: 139, minPow: 50 },
-  { club: "9 Iron", min: 140, max: 159, minPow: 50 },
-  { club: "8 Iron", min: 150, max: 164, minPow: 50 },
-  { club: "7 Iron", min: 165, max: 179, minPow: 50 },
-  { club: "6 Iron", min: 180, max: 200, minPow: 50 },
+  { key: 0, club: "60", min: 5, max: 25, minPow: 50 },
+  { key: 1, club: "56", min: 26, max: 45, minPow: 50 },
+  { key: 2, club: "52", min: 46, max: 80, minPow: 50 },
+  { key: 3, club: "PW", min: 81, max: 139, minPow: 50 },
+  { key: 4, club: "9i", min: 140, max: 159, minPow: 50 },
+  { key: 5, club: "8i", min: 150, max: 164, minPow: 50 },
+  { key: 6, club: "7i", min: 165, max: 179, minPow: 50 },
+  { key: 7, club: "6i", min: 180, max: 200, minPow: 50 },
 ];
 
 const getClub = (distance) => {
