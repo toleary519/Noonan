8edf4b4811d17c695e28b7926cc68e308325d4cd
Thomas O'Leary <t.oleary@me.com>
2021-11-06 16:34:36 -0700

    add fonts

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from "react";
-
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import BagScreenFormat from "./app/screens/BagScreenFormat";
 import ShootScreenFormat from "./app/screens/ShootScreenFormat";
-
+import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { StyleSheet } from "react-native"; //components
 
 const Stack = createMaterialTopTabNavigator();
+
+const getFonts = () =>
+  Font.loadAsync({
+    "Roboto-regular": require("./app/assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-bold": require("./app/assets/fonts/Roboto-Bold.ttf"),
+  });
 
 function MyStack() {
   return (
@@ -34,9 +39,21 @@ function MyStack() {
 }
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (fontLoaded) {
+    return (
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(error) => console.warn(error)}
+      />
+    );
+  }
 }
