207a3dcb08e9635ee8ab673e1a2677c8cf185fbf
Thomas O'Leary <t.oleary@me.com>
2021-11-10 22:08:01 -0800

    add dark colors

--- app/assets/colors/colors.js
+++ app/assets/colors/colors.js
@@ -4,6 +4,10 @@ const colors = {
   blue: "#07A0C3",
   red: "#E76F51",
   brown: "#896C6E",
+  darkGold: "#F2C57C",
+  darkBlack: "#0D1F2D",
+  darkBlue: "#94C5CC",
+  darkRed: "#EF6F6C",
 };
 
 export { colors };
