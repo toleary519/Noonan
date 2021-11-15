94893e04a43f3327907e4a6719cbaca1064f1cea
Thomas O'Leary <t.oleary@me.com>
2021-11-15 11:29:57 -0800

    move firebase functions out to access db

--- app/api/firebaseFunct.js
+++ app/api/firebaseFunct.js
@@ -1,14 +1,30 @@
 import firebase from "firebase/compat/app";
-import { collection, getDocs } from "firebase/compat/firestore";
+import { doc, deleteDoc, setDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
 
-const addShotDB = (newDBclub) => {
-  firebase
-    .firestore()
-    .collection("shots")
-    .add({ ...newDBclub })
-    // .then((snapshot) => snapshot.get())
-    // .then((shotData) => shotData.data())
-    .catch((err) => console.error(err));
-};
+// const addShotDB = (newDBclub) => {
+//   firebase
+//     .firestore()
+//     .collection("shots")
+//     .add({ ...newDBclub })
+//     .catch((err) => console.error(err));
+// };
 
-export { addShotDB };
+// const addShotDB = async (newDBclub) => {
+//   const db = getFirestore();
+//   // Add a new document with a generated id.
+//   const docRef = await addDoc(collection(db, "shots"), { ...newDBclub });
+//   console.log("Document written with ID: ", docRef.id);
+// };
+
+// const deleteShotDB = async (id) => {
+//   await firebase
+//     .firestore()
+//     .collection("shots")
+//     .doc(id)
+//     .delete()
+//     .catch((err) => console.error(err));
+// };
+
+// export { addShotDB };
