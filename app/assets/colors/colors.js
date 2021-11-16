634a4dfecc6e09408e8afad11c64255c3d441849
Thomas O'Leary <t.oleary@me.com>
2021-11-16 13:59:36 -0800

    change reds

--- app/assets/colors/colors.js
+++ app/assets/colors/colors.js
@@ -2,7 +2,7 @@ const colors = {
   bg: "#F5F0E3",
   green: "#5C795D",
   blue: "#07A0C3",
-  red: "#E76F51",
+  red: "#EE6055",
   brown: "#896C6E",
   darkGold: "#F2C57C",
   darkBlack: "#0D1F2D",
