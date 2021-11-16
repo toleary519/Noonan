56dad40f1b950be5034b7364a65482c8f7415f15
Thomas O'Leary <t.oleary@me.com>
2021-11-15 23:28:40 -0800

    hookup calculator to DB

--- app/helpers/calculator.js
+++ app/helpers/calculator.js
@@ -1,27 +1,29 @@
-const shots = [
-  { key: 0, club: "60", min: 5, max: 25, minPow: 50 },
-  { key: 1, club: "56", min: 26, max: 45, minPow: 50 },
-  { key: 2, club: "52", min: 46, max: 80, minPow: 50 },
-  { key: 3, club: "PW", min: 81, max: 139, minPow: 50 },
-  { key: 4, club: "9i", min: 140, max: 159, minPow: 50 },
-  { key: 5, club: "8i", min: 150, max: 164, minPow: 50 },
-  { key: 6, club: "7i", min: 165, max: 179, minPow: 50 },
-  { key: 7, club: "6i", min: 180, max: 200, minPow: 50 },
-];
+// const shots = [
+//   { key: 0, club: "60", min: 5, max: 25, minPow: 50 },
+//   { key: 1, club: "56", min: 26, max: 45, minPow: 50 },
+//   { key: 2, club: "52", min: 46, max: 80, minPow: 50 },
+//   { key: 3, club: "PW", min: 81, max: 139, minPow: 50 },
+//   { key: 4, club: "9i", min: 140, max: 159, minPow: 50 },
+//   { key: 5, club: "8i", min: 150, max: 164, minPow: 50 },
+//   { key: 6, club: "7i", min: 165, max: 179, minPow: 50 },
+//   { key: 7, club: "6i", min: 180, max: 200, minPow: 50 },
+// ];
 
-{
-  /* <NEEDS TO GO TO STORAGE ^^^^^^^^^^^^^^> */
-}
+// {
+//   /* <NEEDS TO GO TO STORAGE ^^^^^^^^^^^^^^> */
+// }
 
-const getClub = (distance) => {
+const getClub = (distance, shots) => {
   const execute = { club: "", power: "" };
 
   for (const shot of shots) {
-    if (distance >= shot.min && distance <= shot.max) {
+    if (distance >= Number(shot.min) && distance <= Number(shot.max)) {
       execute.club = shot.club;
       execute.power = Math.round(
-        shot.minPow +
-          ((100 - shot.minPow) / (shot.max - shot.min)) * (distance - shot.min)
+        Number(shot.minPow) +
+          ((100 - Number(shot.minPow)) /
+            (Number(shot.max) - Number(shot.min))) *
+            (distance - Number(shot.min))
       );
     }
   }
