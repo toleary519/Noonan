a0a04b0890ce7704c9fdf8103549ced6e61217c1
Thomas O'Leary <t.oleary@me.com>
2021-11-19 11:52:09 -0800

    add multi option shot display

--- app/helpers/calculator.js
+++ app/helpers/calculator.js
@@ -5,8 +5,6 @@ const getClub = (actualDistance, shots) => {
     iBOne: "",
     iBTwo: "",
     message: "",
-    club: "",
-    power: "",
   };
 
   const reset = {
@@ -14,18 +12,18 @@ const getClub = (actualDistance, shots) => {
     iBOne: "",
     iBTwo: "",
     message: "",
-    club: "",
-    power: "",
   };
 
   let bagMax = 0;
-  let bagMin = shots[0].min;
+  let bagMin = 0;
 
   // reset the execute object on each pass to eliminate messages and values from previous distance
   execute = { ...reset };
-
   // find the spread of users bag
   for (const shot of shots) {
+    if (bagMin === 0) {
+      bagMin = shots[0].min;
+    }
     if (Number(shot.min) < bagMin) {
       bagMin = shot.min;
       // console.log("bag min - ", bagMin);
@@ -44,33 +42,46 @@ const getClub = (actualDistance, shots) => {
     if (actualDistance < bagMin) {
       execute.message = "You're in close";
     }
-    if (
-      actualDistance >= Number(shot.min) &&
-      actualDistance <= Number(shot.max)
-    ) {
-      // because inputs in bag are strings refactored this with Number(), reset values if found
-      execute = { ...reset };
-      execute.club = shot.club;
-      execute.power = Math.round(
-        Number(shot.minPow) +
-          ((100 - Number(shot.minPow)) /
-            (Number(shot.max) - Number(shot.min))) *
-            (actualDistance - Number(shot.min))
-      );
-    }
   }
 
   //check for inbetween club returns the same
   for (let i = 0; i < shots.length - 1; i++) {
     if (actualDistance > shots[i].max && actualDistance < shots[i + 1].min) {
-      execute = { ...reset };
+      // execute = { ...reset };
       execute.iBOne = shots[i].club;
       execute.iBTwo = shots[i + 1].club;
       execute.iBMessage = "In Between Clubs";
     }
   }
-  // console.log("EXECUTE ::: ", execute);
+  console.log("EXECUTE ::: ", execute);
+
   return execute;
 };
 
-export { getClub };
+const getSwings = (actualDistance, shots) => {
+  let swings = [];
+  let shotObj = { club: "", power: "" };
+
+  for (const shot of shots) {
+    if (
+      actualDistance >= Number(shot.min) &&
+      actualDistance <= Number(shot.max)
+    ) {
+      // because inputs in bag are strings refactored this with Number(), reset values if found
+      // execute = { ...reset };
+      shotObj.club = shot.club;
+      shotObj.power = Math.round(
+        Number(shot.minPow) +
+          ((100 - Number(shot.minPow)) /
+            (Number(shot.max) - Number(shot.min))) *
+            (actualDistance - Number(shot.min))
+      );
+      console.log("shotOBJ: ", { ...shotObj });
+      swings.push({ ...shotObj });
+    }
+    console.log("swings: ", swings);
+  }
+  return swings;
+};
+
+export { getClub, getSwings };
