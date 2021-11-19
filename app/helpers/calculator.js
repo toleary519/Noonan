316c9aac65fc510e36f3da53fb286668eef3d8ea
Thomas O'Leary <t.oleary@me.com>
2021-11-18 16:23:40 -0800

    add out of range and youre in close message handling

--- app/helpers/calculator.js
+++ app/helpers/calculator.js
@@ -1,15 +1,39 @@
-const getClub = (distance, shots) => {
-  const execute = { club: "", power: "" };
+const getClub = (actualDistance, shots) => {
+  const execute = { message: "", club: "", power: "" };
+  let bagMax = 0;
+  let bagMin = shots[0].min;
 
   for (const shot of shots) {
-    // because inputs in bag are text refactored this with Number()
-    if (distance >= Number(shot.min) && distance <= Number(shot.max)) {
+    if (Number(shot.min) < bagMin) {
+      bagMin = shot.min;
+    }
+    if (Number(shot.max) > bagMax) {
+      bagMax = shot.max;
+    }
+  }
+
+  for (const shot of shots) {
+    // check for shots above or below players range
+    if (actualDistance > bagMax) {
+      execute.message = "Out of range";
+
+      return execute;
+    }
+    if (actualDistance < bagMin) {
+      execute.message = "You're in close.";
+      return execute;
+    }
+    if (
+      actualDistance >= Number(shot.min) &&
+      actualDistance <= Number(shot.max)
+    ) {
+      // because inputs in bag are text refactored this with Number()
       execute.club = shot.club;
       execute.power = Math.round(
         Number(shot.minPow) +
           ((100 - Number(shot.minPow)) /
             (Number(shot.max) - Number(shot.min))) *
-            (distance - Number(shot.min))
+            (actualDistance - Number(shot.min))
       );
     }
   }
