6919799aaa5389cb5ebbc082d8801bcba7c45a86
Thomas O'Leary <t.oleary@me.com>
2021-11-21 10:24:41 -0800

    refactor buttons for visible press

--- app/helpers/components/FuncButtons.js
+++ app/helpers/components/FuncButtons.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { colors } from "../../assets/colors/colors";
 
 const ClearButton = ({ text, onPress }) => (
-  <TouchableOpacity>
-    <Text onPress={onPress} style={styles.clearText}>
-      {text}
-    </Text>
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles.clearButton}>
+      <Text style={styles.clearText}>{text}</Text>
+    </View>
   </TouchableOpacity>
 );
 
@@ -20,6 +21,16 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: "bold",
     fontFamily: "Roboto-regular",
+  },
+  clearButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    backgroundColor: colors.darkRed,
+    width: wp("86%"),
+    height: hp("5.5"),
+    left: wp("8%"),
+    borderRadius: 10,
   },
 });
 
