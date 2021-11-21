6df5054f50d106cce1a1961a0f34f478e95bcf27
Thomas O'Leary <t.oleary@me.com>
2021-11-21 10:24:22 -0800

    refactor buttons for visible press

--- app/helpers/components/CalcButton.js
+++ app/helpers/components/CalcButton.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 import { colors } from "../../assets/colors/colors";
 
-export default CalcButton = ({ text, onPress }) => (
-  <TouchableOpacity>
-    <Text onPress={onPress} style={styles.digit}>
-      {text}
-    </Text>
+export default CalcButton = ({ text, onPress, sand, rough }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={sand || rough ? styles.specialButton : styles.calcButton}>
+      <Text style={styles.digit}>{text}</Text>
+    </View>
   </TouchableOpacity>
 );
 
@@ -17,5 +21,31 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "bold",
     fontFamily: "Roboto-regular",
+  },
+  calcButton: {
+    backgroundColor: colors.darkBlue,
+    height: wp("17.6%"),
+    width: wp("17.6%"),
+    borderRadius: 10,
+    marginTop: hp("2.4%"), //20
+    marginLeft: wp("5.3%"), //20
+    marginRight: wp("5.3%"), //20
+    shadowOpacity: 0.7,
+    shadowOffset: { width: wp("-.5%"), height: hp(".5%") },
+    shadowRadius: 2,
+    shadowColor: colors.darkRed,
+  },
+  specialButton: {
+    backgroundColor: colors.darkRed,
+    height: wp("17.6%"),
+    width: wp("17.6%"),
+    borderRadius: 10,
+    marginTop: hp("2.4%"), //20
+    marginLeft: wp("5.3%"), //20
+    marginRight: wp("5.3%"), //20
+    shadowOpacity: 0.7,
+    shadowOffset: { width: wp("-.5%"), height: hp(".5%") },
+    shadowRadius: 2,
+    shadowColor: colors.darkRed,
   },
 });
