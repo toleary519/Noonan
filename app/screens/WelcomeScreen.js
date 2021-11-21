14b7845d1d4f79804ceea8dea6a3f29cfcf19a3c
Thomas O'Leary <t.oleary@me.com>
2021-11-21 01:05:20 -0800

    add info write up

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,6 +5,7 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
   StatusBar,
 } from "react-native";
 import {
@@ -44,9 +45,9 @@ export default function WelcomeScreen({ navigation }) {
             >
               <Text style={styles.text}>Shoot</Text>
             </TouchableOpacity>
-
-            <Text style={[styles.text, { fontSize: hp("4%") }]}>Info</Text>
-
+            <TouchableOpacity onPress={() => setInfo(true)}>
+              <Text style={[styles.text, { fontSize: hp("4%") }]}>Info</Text>
+            </TouchableOpacity>
             <TouchableOpacity onPress={() => setQrCode(true)}>
               <Text style={[styles.text, { fontSize: hp("4%") }]}>qr</Text>
             </TouchableOpacity>
@@ -71,6 +72,117 @@ export default function WelcomeScreen({ navigation }) {
           />
         </View>
       ) : null}
+      {info ? (
+        <ScrollView style={styles.scroll}>
+          <View style={styles.textBlock}>
+            <Text style={styles.titleText}> What is Noonan?</Text>
+            <Text style={styles.bodyText}>
+              Noonan is like a caddy. It is designed to allow you to unlock the
+              full potential of your clubs. Noonan consists of two major
+              screens. Your Bag and The Calculator.{" "}
+            </Text>
+          </View>
+          <View style={styles.textBlock}>
+            <Text style={styles.subtitleText}>Your Bag</Text>
+            <Text style={styles.bodyText}>
+              Start out with your bag. You can add, edit and delete clubs from
+              your bag as you see fit. When adding clubs you will be asked to
+              fill out three values.
+            </Text>
+          </View>
+          <View style={styles.textBlock}>
+            <Text style={styles.subtitleText}>Maximum</Text>
+            <Text style={styles.bodyText}>
+              The max for each club is the distance that you hit that club with
+              a full swing.
+            </Text>
+          </View>
+          <View style={styles.textBlock}>
+            <Text style={styles.subtitleText}>Percent Power</Text>
+            <Text style={styles.bodyText}>
+              The percent power should be the lowest % that you would swing that
+              particular club before you club down.
+            </Text>
+            <Text style={styles.bodyText}>
+              This “spread” isn’t so impressive for clubs like driver and your
+              woods, but grows as you progress through your irons and wedges.
+            </Text>
+            <Text style={[styles.subtitleText, { fontSize: hp("2%") }]}>~</Text>
+            <Text style={styles.bodyText}>
+              For example, let’s say you hit your 9 iron 130 yds but with a a
+              75% swing, “a soft 9,” it goes 110. Your input would be
+            </Text>
+            <Text style={[styles.bodyText, { fontSize: hp("2%") }]}>
+              (Max: 130, % Power: 75, Min: 110)
+            </Text>
+            <Text style={[styles.subtitleText, { fontSize: hp("2%") }]}>~</Text>
+            <Text style={styles.bodyText}>
+              For a 56 degree wedge you may hit it 60 yds with a full swing and
+              10 yds on a 20% swing.
+            </Text>
+            <Text style={[styles.bodyText, { fontSize: hp("2%") }]}>
+              (Max: 60, % Power: 20, Min: 10)
+            </Text>
+            <View style={styles.textBlock}>
+              <Text style={styles.subtitleText}>Minimum</Text>
+              <Text style={styles.bodyText}>
+                As above, your Min distance is the distance you hit the ball on
+                your most conservative swing with that club.
+              </Text>
+            </View>
+            <View style={styles.textBlock}>
+              <Text style={styles.subtitleText}>Other Things</Text>
+              <Text style={styles.bodyText}>
+                If you don’t know these numbers off the top of your head that is
+                okay! Clubs can be updated by clicking on the edit button. You
+                can do this on the fly and the calculator will adapt in
+                realtime.
+              </Text>
+              <Text style={[styles.subtitleText, { fontSize: hp("2%") }]}>
+                ~
+              </Text>
+              <Text style={styles.bodyText}>
+                Make your best guess and if you realize on the range or course
+                that you actually hit 10-15 yds further than you thought, great
+                news! Change the values and let Noonan keep track.
+              </Text>
+              <Text style={styles.bodyText}>
+                By doing this you will get a better sense of how dynamic your
+                clubs really are. Fill out your bag and then you can use …
+              </Text>
+            </View>
+            <View style={styles.textBlock}>
+              <Text style={styles.subtitleText}> The Calculator </Text>
+              <Text style={styles.bodyText}>
+                The calculator uses the information provided in your Bag to
+                retrieve which club at which power you should hit for a given
+                distance. This data is unique to you and your clubs.
+              </Text>
+              <Text style={styles.bodyText}>
+                Use the keys at the bottom to enter the yardage. "S" indicates
+                SAND and "R" indicates ROUGH.
+              </Text>
+              <Text style={styles.bodyText}>
+                You can also adjust your estimated elevation and wind. Elevation
+                up and down is in yds and wind (arrow down = head wind, arrow up
+                is tail wind) in mph.
+              </Text>
+              <Text style={[styles.subtitleText, { fontSize: hp("2%") }]}>
+                ~
+              </Text>
+              <Text style={styles.bodyText}>It's about having fun!</Text>
+              <Text style={styles.bodyText}> So give it a whirl.</Text>
+            </View>
+            <AntDesign
+              onPress={() => setInfo(false)}
+              name="closecircleo"
+              size={45}
+              color={colors.darkBlue}
+              style={{ marginLeft: wp("45%"), marginVertical: hp("4%") }}
+            />
+          </View>
+        </ScrollView>
+      ) : null}
     </View>
   );
 }
@@ -79,7 +191,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "center",
     backgroundColor: colors.darkBlack,
   },
   text: {
@@ -100,5 +212,45 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: colors.darkBlack,
+  },
+  textBlock: {
+    flex: 1,
+    // borderWidth: 1,
+    // borderColor: colors.green,
+    // borderRadius: 6,
+    // elevation: 3,
+    // shadowOffset: { width: 1, height: 1 },
+    // shadowColor: colors.bg,
+    // shadowOpacity: 0.3,
+    // marginHorizontal: 4,
+    // marginVertical: 6,
+    justifyContent: "flex-start",
+  },
+  titleText: {
+    textAlign: "center",
+    fontSize: hp("5%"),
+    marginTop: hp("4%"),
+    color: colors.darkGold,
+    fontFamily: "Yellow-tail",
+    textShadowOffset: { width: wp("-.3%"), height: hp(".3%") },
+    textShadowRadius: 2,
+    textShadowColor: colors.darkRed,
+  },
+  subtitleText: {
+    textAlign: "center",
+    color: colors.darkGold,
+    fontSize: hp("4"),
+    fontFamily: "Yellow-tail",
+    textShadowOffset: { width: wp("-.3%"), height: hp(".3%") },
+    textShadowRadius: 2,
+    textShadowColor: colors.darkRed,
+  },
+
+  bodyText: {
+    textAlign: "center",
+    fontSize: hp("2.3%"),
+    color: colors.bg,
+    margin: hp("2%"),
+    fontFamily: "Roboto-regular",
   },
 });
