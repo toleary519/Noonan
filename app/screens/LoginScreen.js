82ffcfa6d1880bfc09fe8340e8ab6c956056b42d
Thomas O'Leary <t.oleary@me.com>
2021-12-27 21:45:50 -0800

    comments

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -66,7 +66,7 @@ const LoginScreen = ({ navigation }) => {
 
     signInWithRedirect(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
+        // This gives you a Google Access Token.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
         // The signed-in user info.
